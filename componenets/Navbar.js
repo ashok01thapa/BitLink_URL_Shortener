1b88ee201b124b6b9d21476aba33df268d2e35ae
ashok01thapa <153058399+ashok01thapa@users.noreply.github.com>
2025-04-02 18:56:26 +0545

    Update Navbar.js

--- componenets/Navbar.js
+++ componenets/Navbar.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import Link from 'next/link'
+
 const Navbar = () => {
   return (
-    <nav className='h-18 bg-purple-700 flex justify-between px-10 items-center text-white'>
+    <nav className='h-18 bg-purple-700 flex justify-between md:justify-between  items-center text-white'>
         <div className="logo font-bold text-lg">
             <Link href="/">BitLinks</Link>
         </div>
@@ -12,8 +13,8 @@ const Navbar = () => {
             <Link href="/shorten"><li>Shorten</li></Link>
             <Link href="/Contact"><li>Contact Us</li></Link>
             <li className='flex gap-3'>
-                <Link href="/shorten"><button className='bg-purple-500 shadow-lg rounded-lg py-1 font-bold cursor-pointer p-3'>Try Now</button></Link>
-                <Link href="/github"><button className='bg-purple-500 shadow-lg rounded-lg py-1 font-bold  cursor-pointer p-3'>GitHub</button></Link>
+                <Link href="/shorten"><button className='md:flex-row hidden md:block bg-purple-500 shadow-lg rounded-lg py-1 font-bold cursor-pointer p-3'>Try Now</button></Link>
+                <Link href="/github"><button className='md:flex-row hidden md:block bg-purple-500 shadow-lg rounded-lg py-1 font-bold  cursor-pointer p-3'>GitHub</button></Link>
             </li>
         </ul>
     </nav>
